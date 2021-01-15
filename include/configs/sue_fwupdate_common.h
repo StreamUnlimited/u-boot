StreamUnlimited/u-boot

commit 6258118200432d9b7397d37405f7a72461265a13

configs: sue_fwupdate_common: swu-fit fallbacks to normal image [STREAMEIGHT-2166][STREAMEIGHT-4489]
The board now boots back to normal image if swu-fit image is not
loaded or does not boot and fails for three consecutive times.

Signed-off-by: André Groenewald <andre.groenewald@streamunlimited.com>
[polished commit message]
[whitespace cleanup moved to previous commit]
[insert missing whitespace after "fi; "]
Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>
[backported from master-stream183x commit 93000a39350f158160230b1a617012a7fdf6568e]
Signed-off-by: Quentin Schulz <quentin.schulz@streamunlimited.com>
[use spaces instead of tabs to be consistent with rest of the code]
Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

include/configs/sue_fwupdate_common.h
@@ -105,21 +105,29 @@
         "else " \
             "echo \"INFO: Bootcount != 1, not checking factory state or USB update request\"; " \
         "fi; " \
-        "if fwup fail; " \
+        "if fwup update; " \
             "then " \
             "min_boot_retry=3;" \
             "if test \"${bootcount}\" -gt ${min_boot_retry}; " \
                 "then " \
+                    "echo \"INFO: Update flag is set, bootcount is greater than ${min_boot_retry}\"; " \
+                    "swufail=yes; " \
+                "else " \
+                    "echo \"INFO: Update flag is set but bootcount not greater than ${min_boot_retry}\"; " \
+                    "boot_swupdate=yes; " \
+                "fi; " \
+        "else " \
+            "if fwup fail; " \
+                "then " \
+                "min_boot_retry=3;" \
+                "if test \"${bootcount}\" -gt ${min_boot_retry}; " \
+                    "then " \
                     "echo \"INFO: Fail flag is set, bootcount is greater than ${min_boot_retry}\"; " \
                     "boot_swupdate=yes; " \
                 "else " \
                     "echo \"INFO: Fail flag is set but bootcount not greater than ${min_boot_retry}\"; " \
                 "fi; " \
-        "fi; " \
-        "if fwup update; " \
-            "then " \
-            "echo \"INFO: Update flag is set\"; " \
-            "boot_swupdate=yes; " \
+            "fi; " \
         "fi; " \
         "if test ${boot_swupdate} = yes; " \
             "then " \
@@ -173,6 +181,10 @@
     "mmcroot=/dev/mmcblk2p6 rw\0" \
     "mmcrootfstype=ext4\0" \
     "flashargs=run kernel_common_args; " \
+        "if test ${swufail} = yes; " \
+            "then " \
+            "setenv bootargs ${bootargs} swufail; " \
+        "fi; " \
         "setenv bootargs ${bootargs} " \
         "root=${mmcroot} rootwait noinitrd " \
         "rootfstype=${mmcrootfstype};\0" \

commit 23d879f5b6c37a4df265f3fc31399c9427961858

configs: sue_fwupdate_common: properly initialize swufail [STREAMEIGHT-3497][STREAMEIGHT-4489]
If swufail is not properly initialized, the condition
"if test ${swufail} = yes;" evaluates as true, causing that swufail is
always present.

This fixes commit 6e58bda1f763f4bace882e2d53346622d42e7b9f

Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>
[backported from master-stream183x commit c84c9423065dc38aaaf55218c1b3c05890da9d52]
[fixes: 6258118200432d9b7397d37405f7a72461265a13]
Signed-off-by: Quentin Schulz <quentin.schulz@streamunlimited.com>
[use spaces instead of tabs as in original commit]
Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

include/configs/sue_fwupdate_common.h
@@ -88,6 +88,7 @@
             "fi; " \
         "fi; " \
         "boot_swupdate=no; " \
+        "swufail=no; " \
         "if test ${bootcount} -eq 1; " \
             "then " \
             "echo \"INFO: Bootcount = 1, checking if board is in factory state or USB update request is active\"; " \