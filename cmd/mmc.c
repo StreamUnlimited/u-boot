b833d8e073c68ae4f73a8412da17c35fe7492c2b
Jaehoon Chung <jh80.chung@samsung.com>
2021-09-24 09:23:34 +0900

    cmd: mmc: check whether card is SD or eMMC before hwpartition
    commit 0d453c84bae95174ca3bd94c72a46112580b24c8 upstream.
    
    It doesn't need to follow more sequence to do the hwparititioning,
    because SD doesn't support hwpartitioning feature.
    
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Jaehoon Chung <jh80.chung@samsung.com>
    Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

--- cmd/mmc.c
+++ cmd/mmc.c
@@ -553,6 +553,11 @@ static int do_mmc_hwpartition(cmd_tbl_t *cmdtp, int flag,
 	if (!mmc)
 		return CMD_RET_FAILURE;
 
+	if (IS_SD(mmc)) {
+		puts("SD doesn't support partitioning\n");
+		return CMD_RET_FAILURE;
+	}
+
 	if (argc < 1)
 		return CMD_RET_USAGE;
 	i = 1;
