StreamUnlimited/u-boot

commit 61418654a36632a045b7e8c46ba94919c9a8f617

support new AXP313A PMIC [STREAMEIGHT-4948]
AXP313A supports two I2C slave addresses (0x36 and 0x37).
The address used on Stream810X3 is 0x36.

*enable DLDO 3.3V
*enable regulators power off delay
*enable regulators first on last off sequence

Signed-off-by: Wei Chen <wei.chen@streamunlimited.com>
[fixed coding style]
[fixed file headers]
[do not print extra fail sequences in addition to axp313_write/read]
Signed-off-by: Radek Dostál <radek.dostal@streamunlimited.com>

board/streamunlimited/stream1832/stream1832.c
@@ -53,6 +53,10 @@
 #include <axp152.h>
 #endif
 
+#ifdef CONFIG_AXP313_POWER
+#include <axp313.h>
+#endif
+
 #include <asm/reboot.h>
 
 #include "../common/fwupdate.h"
@@ -462,13 +466,6 @@ int board_axp152_init(void)
 {
 	int ret = 0;
 
-	/* first try to find the AXP at addr 0x32 */
-	ret = axp152_init(0x32);
-	if (ret) {
-		/* if this fails we try to use 0x30, which was a hardware bug on older modules */
-		ret = axp152_init(0x30);
-	}
-
 	/*
 	 * Set the DCDC workmode of the DCDC2 (VDDCPU/VDD_ARM) to PWM only mode. The reason is
 	 * that per default the board boots in 1.2 GHz mode which requires the PWM only mode.
@@ -510,6 +507,56 @@ int board_axp152_init(void)
 	ret |= axp152_set_gpio2_ldo(3300);
 
 	printf("AXP152 init done: %d\n", ret);
+
+	return ret;
+}
+#endif
+
+#ifdef CONFIG_AXP313_POWER
+int board_axp313_init(void)
+{
+	int ret = 0;
+
+	/*
+	 * Set the DCDC workmode of the DCDC2 (VDDCPU/VDD_ARM) to PWM only mode. The reason is
+	 * that per default the board boots in 1.2 GHz mode which requires the PWM only mode.
+	 * The automode leads to stability issues on 1.2 GHz and up. The workmode for lower
+	 * operating points will be handled by the kernel.
+	 */
+	ret |= axp313_set_dcdc_workmode(AXP313_DCDC1, AXP313_DCDC_WORKMODE_PWM);
+
+	/*
+	 * Also reset DCDC3 to 1.8 V ,This is also the default value of the AXP313 after power on.
+	 */
+	ret |= axp313_set_dcdc3(1800);
+
+	/* Set DCDC1 to 1.１V */
+	ret |= axp313_set_dcdc1(1100);
+
+	/* Set VDD_EE to 0.95V */
+	ret |= axp313_set_dcdc2(1050);
+
+	/* Set ALDO to 3.1V */
+	ret |= axp313_set_aldo1(AXP313_ALDO_3V3);
+
+	/* Set DLDO to 3.3V */
+	ret |= axp313_set_dldo1(AXP313_ALDO_3V3);
+
+	/* Set power off sequence 1BH bi3 to 1 first open last off */
+	ret |= axp313_set_poweroff_first_open_last_off();
+
+	/*
+	 * Always cycle WIFI_VRF otherwise the chip might be in some inconsistent state
+	 *
+	 * After doing the measurements with the HW department, we have to wait for 50 ms
+	 * before the power rails are cleanly at 0.
+	 * AX313A, it use PIN_GPIOZ_10(WIFI_VREF_ON) to control WIFI_VREF
+	 */
+	amlogic_wifi_vrf_disable();
+	udelay(50000);
+	amlogic_wifi_vrf_enable();
+
+	printf("AXP313 init done: %d\n", ret);
 
 	return ret;
 }
@@ -573,13 +620,7 @@ static int reset_cause(void)
 
 int board_init(void)
 {
-#ifdef CONFIG_SYS_I2C_AML
-	board_i2c_init();
-#endif
-
-#ifdef CONFIG_AXP152_POWER
-	board_axp152_init();
-#endif
+	int ret;
 	/*
 	 * After the AXP was initialized the CPU voltage will be at 1.1 V, so we can
 	 * set back the SYS_PLL configuration from 672 MHz (0xC0020270) to
@@ -593,6 +634,33 @@ int board_init(void)
 
 	sue_device_detect(&current_device);
 
+#ifdef CONFIG_SYS_I2C_AML
+	board_i2c_init();
+#endif
+
+	if (current_device.module_version < 4) {
+		/* for Stream1832 L1, L2 and L3 we first try to find the AXP152 at addr 0x32 */
+		ret = axp152_init(0x32);
+		if (!ret) {
+			board_axp152_init();
+		} else {
+			/* if this fails we try to use 0x30, which was a hardware bug on older modules */
+			ret = axp152_init(0x30);
+			if (!ret) {
+				board_axp152_init();
+			} else {
+				printf("Error, i2c failed to communicate with PMIC AXP152, i2c slave address 0x30 : %d\n", ret);
+			}
+		}
+	} else {
+		/* for Stream1832 L4 and higher we try to find the AXP313 only at addr 0x36 */
+		ret = axp313_init(0x36);
+		if (!ret) {
+			board_axp313_init();
+		} else {
+			printf("Error, i2c failed to communicate with PMIC AXP313A,i2c slave address 0x36 : %d\n", ret);
+		}
+	}
 #ifdef CONFIG_AML_V2_FACTORY_BURN
 	aml_try_factory_usb_burning(0, gd->bd);
 #endif// #ifdef CONFIG_AML_V2_FACTORY_BURN

drivers/power/Makefile
@@ -7,6 +7,7 @@
 
 obj-$(CONFIG_AS3722_POWER)	+= as3722.o
 obj-$(CONFIG_AXP152_POWER)	+= axp152.o
+obj-$(CONFIG_AXP313_POWER)	+= axp313.o
 obj-$(CONFIG_AXP209_POWER)	+= axp209.o
 obj-$(CONFIG_AXP221_POWER)	+= axp221.o
 obj-$(CONFIG_EXYNOS_TMU)	+= exynos-tmu.o

drivers/power/axp313.c
@@ -1,0 +1,224 @@
+/*
+ * AXP313A PMIC u-boot driver.
+ *
+ * (C) Copyright 2022 StreamUnlimited
+ * Author: Wei Chen <wei.chen@streamunlimited.com>
+ *
+ * Functions and registers to access AXP313A power management chip. *
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <i2c.h>
+#include <axp313.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/gpio.h>
+
+#define CONFIG_WIFI_VEER_GPIO  PIN_GPIOZ_10
+#define CONFIG_WIFI_VEER_GPIO_NAME "GPIOZ_10"
+
+enum axp313_reg {
+        AXP313A_POWER_ON_SOURCE_INDIVATION			 = 0x00,
+        AXP313A_POWER_OFF_SOURCE_INDIVATION			 = 0x01,
+        AXP313A_VERSION						 = 0x03,
+        AXP313A_OUTPUT_POWER_ON_OFF_CTL				 = 0x10,
+        AXP313A_DCDC_DVM_PWM_CTL				 = 0x12,
+        AXP313A_DC1OUT_VOL					 = 0x13,
+        AXP313A_DC2OUT_VOL          				 = 0x14,
+        AXP313A_DC3OUT_VOL          				 = 0x15,
+        AXP313A_ALDO1OUT_VOL					 = 0x16,
+        AXP313A_DLDO1OUT_VOL					 = 0x17,
+        AXP313A_POWER_DOMN_SEQUENCE				 = 0x1A,
+        AXP313A_PWROK_VOFF_SERT					 = 0x1B,
+        AXP313A_POWER_WAKEUP_CTL				 = 0x1C,
+        AXP313A_OUTPUT_MONITOR_CONTROL				 = 0x1D,
+        AXP313A_POK_SET						 = 0x1E,
+        AXP313A_IRQ_ENABLE					 = 0x20,
+        AXP313A_IRQ_STATUS					 = 0x21,
+};
+
+#define AXP313_POWEROFF			(1 << 7)
+
+static u8 axp313_addr;
+
+static int axp313_write(enum axp313_reg reg, u8 val)
+{
+	int ret;
+	ret = i2c_write(axp313_addr, reg, 1, &val, 1);
+	if (ret)
+		printf("axp313_write failed, ret= %d\n", ret);
+#ifdef DEBUG
+	else
+		printf("axp313_write reg=0x%x , val=0x%x\n", reg,(unsigned char)(val & 0xFF));
+#endif
+	return ret;
+}
+
+static int axp313_read(enum axp313_reg reg, u8 *val)
+{
+	int ret;
+	ret = i2c_read(axp313_addr, reg, 1, val, 1);
+	if (ret)
+		printf("axp313_read failed, ret= %d\n", ret);
+#ifdef DEBUG
+	else
+		printf("axp313_read reg=0x%x , val=0x%x\n", reg,(unsigned char)(*val & 0xFF));
+#endif
+	return ret;
+}
+
+static u8 axp313_mvolt_to_target(int mvolt, int min, int max, int div)
+{
+	if (mvolt < min)
+		mvolt = min;
+	else if (mvolt > max)
+		mvolt = max;
+
+	return (mvolt - min) / div;
+}
+
+int axp313_set_aldo1(enum axp313_aldo_voltage aldo_val)
+{
+	int ret;
+	u8 current;
+
+	ret = axp313_read(AXP313A_ALDO1OUT_VOL, &current);
+	if (ret)
+		return ret;
+	current &= 0xC0;
+	current |= aldo_val & 0x1F;
+	return axp313_write(AXP313A_ALDO1OUT_VOL, current);
+}
+
+int axp313_set_dldo1(enum axp313_aldo_voltage dldo_val)
+{
+	int ret;
+	u8 current, power_ctl;
+
+	ret = axp313_read(AXP313A_OUTPUT_POWER_ON_OFF_CTL, &power_ctl);
+	if (ret)
+		return ret;
+	power_ctl |= (1 << 4);
+	ret = axp313_write(AXP313A_OUTPUT_POWER_ON_OFF_CTL, power_ctl);
+	if (ret)
+		return ret;
+
+	ret = axp313_read(AXP313A_DLDO1OUT_VOL, &current);
+	if (ret)
+		return ret;
+	current &= 0xC0;
+	current |= dldo_val & 0x1F;
+	return axp313_write(AXP313A_DLDO1OUT_VOL, current);
+}
+
+int axp313_set_dcdc1(int mvolt)
+{
+	u8 target = axp313_mvolt_to_target(mvolt, 500, 1200, 10);
+
+	return axp313_write(AXP313A_DC1OUT_VOL, target);
+}
+
+int axp313_set_dcdc2(int mvolt)
+{
+	int rc;
+	u8 current, target;
+
+	target = axp313_mvolt_to_target(mvolt, 500, 1200, 10);
+
+	/* Do we really need to be this gentle? It has built-in voltage slope */
+	while ((rc = axp313_read(AXP313A_DC2OUT_VOL, &current)) == 0 &&
+	       current != target) {
+		if (current < target)
+			current++;
+		else
+			current--;
+		rc = axp313_write(AXP313A_DC2OUT_VOL, current);
+		if (rc)
+			break;
+	}
+	return rc;
+}
+
+int axp313_set_dcdc3(int mvolt)
+{
+	u8 target = axp313_mvolt_to_target(mvolt, 1220, 1840, 20);
+	// DCDC3 register AXP313A_DC3OUT_VOL 0x15 ,1.22V, value 0x1D
+	target += 0x47;
+	return axp313_write(AXP313A_DC3OUT_VOL, target);
+}
+
+int axp313_set_dcdc_workmode(enum axp313_dcdc_regulator id, int mode)
+{
+	u8 reg;
+	int ret;
+
+	if (id < AXP313_DCDC1 || id > AXP313_DCDC3)
+		return -EINVAL;
+
+	ret = axp313_read(AXP313A_DCDC_DVM_PWM_CTL, &reg);
+	if (ret)
+		return ret;
+
+	reg = reg & ~(1 << (AXP313_DCDC3 - id));
+	reg |= mode << (AXP313_DCDC3 - id);
+
+	return axp313_write(AXP313A_DCDC_DVM_PWM_CTL, reg);
+}
+
+int axp313_init(unsigned char i2c_addr)
+{
+	u8 ver;
+	int rc;
+
+	axp313_addr = i2c_addr;
+
+	rc = axp313_read(AXP313A_VERSION, &ver);
+	if (rc)
+		return rc;
+
+	if (ver != 0x4B)
+		return -1;
+
+	return 0;
+}
+
+int axp313_set_poweroff_first_open_last_off(void)
+{
+	u8 reg;
+	int ret;
+
+	ret = axp313_read(AXP313A_PWROK_VOFF_SERT, &reg);
+	if (ret)
+		return ret;
+	reg |= (1 << 3);//set power off sequence=1 0x1B bit 3
+	reg |= (1 << 2);//set power off regulaters delay=1 0x1B bit 2
+	return axp313_write(AXP313A_PWROK_VOFF_SERT, reg);
+}
+
+void amlogic_wifi_vrf_enable(void)
+{
+	int ret;
+
+	ret = gpio_request(CONFIG_WIFI_VEER_GPIO,CONFIG_WIFI_VEER_GPIO_NAME);
+
+	if (ret && ret != -EBUSY) {
+		printf("gpio: requesting pin %u failed\n",
+			CONFIG_WIFI_VEER_GPIO);
+		return;
+	}
+	gpio_direction_output(CONFIG_WIFI_VEER_GPIO, 1);
+}
+
+void amlogic_wifi_vrf_disable(void)
+{
+	int ret;
+
+	ret = gpio_request(CONFIG_WIFI_VEER_GPIO,CONFIG_WIFI_VEER_GPIO_NAME);
+	if (ret && ret != -EBUSY) {
+		printf("gpio: requesting pin %u failed\n",
+			CONFIG_WIFI_VEER_GPIO);
+		return;
+	}
+	gpio_direction_output(CONFIG_WIFI_VEER_GPIO, 0);
+}

include/axp313.h
@@ -1,0 +1,65 @@
+/*
+ * AXP313A PMIC u-boot driver.
+ *
+ * (C) Copyright 2022 StreamUnlimited
+ * Author: Wei Chen <wei.chen@streamunlimited.com>
+ *
+ * Functions and registers to access AXP313A power management chip. *
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+
+enum axp313_aldo_voltage {
+	AXP313_ALDO_0V5	= 0x00,
+	AXP313_ALDO_0V6	= 0x01,
+	AXP313_ALDO_0V7	= 0x02,
+	AXP313_ALDO_0V8	= 0x03,
+	AXP313_ALDO_0V9	= 0x04,
+	AXP313_ALDO_1V0	= 0x05,
+	AXP313_ALDO_1V1	= 0x06,
+	AXP313_ALDO_1V2	= 0x07,
+	AXP313_ALDO_1V3	= 0x08,
+	AXP313_ALDO_1V4	= 0x09,
+	AXP313_ALDO_1V5	= 0x0A,
+	AXP313_ALDO_1V6	= 0x0B,
+	AXP313_ALDO_1V7	= 0x0C,
+	AXP313_ALDO_1V8	= 0x0D,
+	AXP313_ALDO_1V9	= 0x0E,
+	AXP313_ALDO_2V0	= 0x0F,
+	AXP313_ALDO_2V1	= 0x10,
+	AXP313_ALDO_2V2	= 0x11,
+	AXP313_ALDO_2V3	= 0x12,
+	AXP313_ALDO_2V4	= 0x13,
+	AXP313_ALDO_2V5	= 0x14,
+	AXP313_ALDO_2V6	= 0x15,
+	AXP313_ALDO_2V7	= 0x16,
+	AXP313_ALDO_2V8	= 0x17,
+	AXP313_ALDO_2V9	= 0x18,
+	AXP313_ALDO_3V0	= 0x19,
+	AXP313_ALDO_3V1	= 0x1A,
+	AXP313_ALDO_3V2	= 0x1B,
+	AXP313_ALDO_3V3	= 0x1C,
+	AXP313_ALDO_3V4	= 0x1D,
+	AXP313_ALDO_3V5	= 0x1E,
+};
+
+#define AXP313_DCDC_WORKMODE_AUTO	0
+#define AXP313_DCDC_WORKMODE_PWM	1
+
+enum axp313_dcdc_regulator {
+	AXP313_DCDC1 = 0,
+	AXP313_DCDC2,
+	AXP313_DCDC3,
+};
+
+int axp313_set_dcdc_workmode(enum axp313_dcdc_regulator reg, int mode);
+int axp313_set_dcdc1(int mvolt);
+int axp313_set_dcdc2(int mvolt);
+int axp313_set_dcdc3(int mvolt);
+int axp313_set_aldo1(enum axp313_aldo_voltage aldo_val);
+int axp313_set_dldo1(enum axp313_aldo_voltage aldo_val);
+int axp313_init(unsigned char i2c_addr);
+int axp313_set_poweroff_first_open_last_off(void);
+void amlogic_wifi_vrf_enable(void);
+void amlogic_wifi_vrf_disable(void);

include/configs/stream1832.h
@@ -573,6 +573,9 @@
 
 
 #define CONFIG_AXP152_POWER
+
+#define CONFIG_AXP313_POWER
+
 #define CONFIG_OF_BOARD_SETUP
 
 #endif

commit b791dd7373302362867690a83f3d04d268e19c07

board: sue: stream1832: set AXP313 PMIC VDD_EE to 0.95V [STREAMEIGHT-5699]
The old regulator set the correct voltage, but the new one sets it higher.
This is not needed and consumes more power. This patch makes the PMIC variants
more consistent.

Fixes: 61418654a366 ("support new AXP313A PMIC [STREAMEIGHT-4948]")

Signed-off-by: Peter Suti <peter.suti@streamunlimited.com>

board/streamunlimited/stream1832/stream1832.c
@@ -534,7 +534,7 @@ int board_axp313_init(void)
 	ret |= axp313_set_dcdc1(1100);
 
 	/* Set VDD_EE to 0.95V */
-	ret |= axp313_set_dcdc2(1050);
+	ret |= axp313_set_dcdc2(950);
 
 	/* Set ALDO to 3.1V */
 	ret |= axp313_set_aldo1(AXP313_ALDO_3V3);