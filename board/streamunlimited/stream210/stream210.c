10db89968f5520c54bb462310331851f3b06ff9c
Martin Pietryka <martin.pietryka@streamunlimited.com>
2024-07-25 18:35:18 +0200

    board: sue: stream210: add reset_misc() implementation [STREAMEIGHT-7175]
    This was code was taken over from `board/realtek/amebasmart/ameba.c`
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- board/streamunlimited/stream210/stream210.c
+++ board/streamunlimited/stream210/stream210.c
@@ -41,6 +41,18 @@ void reset_cpu(ulong addr)
 {
 }
 
+void reset_misc(void)
+{
+	writel(0U, SYSTEM_CTRL_BASE_LP + REG_AON_SYSRST_MSK);
+	writel(0U, SYSTEM_CTRL_BASE_LP + REG_LSYS_SYSRST_MSK0);
+	writel(0U, SYSTEM_CTRL_BASE_LP + REG_LSYS_SYSRST_MSK1);
+	writel(0U, SYSTEM_CTRL_BASE_LP + REG_LSYS_SYSRST_MSK2);
+
+	writel(SYS_RESET_KEY, SYSTEM_CTRL_BASE_LP + REG_LSYS_SW_RST_TRIG);
+	writel(LSYS_BIT_LPSYS_RST << AP_CPU_ID, SYSTEM_CTRL_BASE_LP + REG_LSYS_SW_RST_CTRL);
+	writel(SYS_RESET_TRIG, SYSTEM_CTRL_BASE_LP + REG_LSYS_SW_RST_TRIG);
+}
+
 bool is_sue_secureboot(void)
 {
 	u32 reg = 0;
