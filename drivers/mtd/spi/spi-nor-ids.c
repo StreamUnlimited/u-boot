4a4d62cb37cb8627f19706c55be6bc0f31a650b9
anne_yu <anne_yu@realsil.com.cn>
2025-12-19 15:08:52 +0800

    feat(spi): support gd25q256 nor flash
    Change-Id: I07839022c42f0dbe3d15caee890e7ce5bb1d1c67

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -117,6 +117,12 @@ const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_DUAL_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
+	{
+		INFO("gd25q256", 0xc84019, 0, 64 * 1024, 512,
+			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
+			SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SPI_NOR_4B_OPCODES)
+	},
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	/* ISSI */
